8dc96080dbc5ef9d2b7e4ed735662452ffaf3512
Austin McIntyre <aumcintyre@gmail.com>
2023-10-26 13:59:42 -0500

    finished BTS, working on Data Structures

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -32,37 +32,183 @@ calcAge(1991); */
 //HOISTING LECTURE
 
 //Variables
-console.log(me);
+// console.log(me);
 // console.log(job);
 // console.log(year);
 
-var me = 'Austin';
-let job = 'Teacher';
-const year = 1991;
+// var me = 'Austin';
+// let job = 'Teacher';
+// const year = 1991;
 
 //Functions
-console.log(addDecl(2, 3));
+// console.log(addDecl(2, 3));
 // console.log(addExpr(2, 3));
 // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-    return a + b;
-}
+// function addDecl(a, b) {
+//     return a + b;
+// }
 
-const addExpr = function (a, b) {
-    return a + b;
-}
+// const addExpr = function (a, b) {
+//     return a + b;
+// }
 
-const addArrow = (a, b) => a + b;
+// const addArrow = (a, b) => a + b;
 
 //Example:
 
-if (!numProducts) {
-    deleteShoppingCart();
-}
+// if (!numProducts) {
+//     deleteShoppingCart();
+// }
 
-var numProducts = 10;
+// var numProducts = 10;
 
-function deleteShoppingCart() {
-    console.log('All products deleted!');
-}
+// function deleteShoppingCart() {
+//     console.log('All products deleted!');
+// }
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//     console.log(2023 - birthYear);
+//     console.log(this);
+// }
+
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//     console.log(2023 - birthYear);
+//     console.log(this);
+// }
+
+// calcAgeArrow(1991);
+
+// const austin = {
+//     year: 1991,
+//     calcAge: function (birthYear) {
+//         console.log(this);
+//         console.log(2023 - this.year);
+//     }
+// }
+
+// austin.calcAge();
+
+// const matilda = {
+//     year: 2017,
+// }
+
+// matilda.calcAge = austin.calcAge;
+// matilda.calcAge();
+
+// const f = austin.calcAge;
+// f();
+
+// const austin = {
+//     firstName: 'Austin',
+//     year: 1991,
+//     calcAge: function (birthYear) {
+//         // console.log(this);
+//         console.log(2023 - this.year);
+
+//         //SOLUTION #1
+//         // const self = this // 'self' OR 'that'
+//         // const isMillenial = function () {
+//         //     console.log(self);
+//         //     // console.log(this.year >= 1981 && this.year <= 1996);
+//         //     console.log(self.year >= 1981 && self.year <= 1996);
+//         // };
+
+//         //SOLUTION #2 (ES6 or Newer)
+//         const isMillenial = () => {
+//             console.log(this);
+//             // console.log(this.year >= 1981 && this.year <= 1996);
+//             console.log(this.year >= 1981 && this.year <= 1996);
+//         };
+
+//         isMillenial();
+//     },
+//     greet: function () { console.log(`Hey ${this.firstName}`) },
+// }
+
+
+// austin.greet();
+// austin.calcAge();
+
+
+//ARGUMENTS keyword
+// function addDecl(a, b) {
+//     console.log(arguments);
+//     return a + b;
+// }
+
+// addDecl(2, 5);
+
+// const addExpr = function (a, b) {
+//     console.log(arguments);
+//     return a + b;
+// }
+// addExpr(2, 5);
+
+// const addArrow = (a, b) => {
+//     console.log(arguments);
+//     return a + b
+// };
+
+// addArrow(2, 5, 8);
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// 
+// console.log(age);
+// console.log(oldAge);
+// 
+// const me = {
+// name: 'Austin',
+// age: 30,
+// }
+// 
+// const friend = me;
+// 
+// friend.age = 29;
+// 
+// console.log(friend);
+// console.log(me);
+
+//Primitive Types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+
+console.log(lastName);
+console.log(oldLastName);
+
+//Reference Types
+const jessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+
+console.log('Before Marriage', jessica);
+console.log('After Marriage', marriedJessica);
+
+//Copying objects
+const jessica2 = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Jim', 'Jon']
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Beth');
+jessicaCopy.family.push('Mary');
+
+console.log('Before Marriage', jessica2);
+console.log('After Marriage', jessicaCopy);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,23 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
+
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(`Order Received. ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}!`);
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +44,166 @@ const restaurant = {
     },
   },
 };
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags } = restaurant;
+console.log(restaurantName, hours, tags);
+
+//Default Values
+const { menu1 = [], starterMenu: starters = [] } = restaurant;
+console.log(menu1, starters);
+
+//Mutating Variables
+/*
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+
+({ a, b } = obj);
+console.log(a, b);
+*/
+
+//Nested Object Destructuring
+const { fri: { open: friOpen, close: friClose } } = openingHours;
+console.log(friOpen, friClose);
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sol, 21',
+  mainIndex: 2,
+  starterIndex: 2
+})
+
+restaurant.orderDelivery({
+  address: 'Via del Sol, 21',
+  starterIndex: '1'
+})
+
+//------------------------ARRAY DESTRUCTURING
+// const arr = [2, 3, 4];
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+
+// const [x, y, z] = arr;
+
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [main, , secondary] = restaurant.categories;
+// console.log(main, secondary);
+
+// [secondary, main] = [main, secondary];
+// console.log(main, secondary);
+
+// const [starter, mainCourse] = restaurant.order(2, 0);
+// console.log(starter, mainCourse);
+
+// const nested = [2, 4, [5, 6]];
+// // const [i, , j] = nested;
+// // console.log(i, j);
+// const [i, , [j, h]] = nested;
+// console.log(i, j, h );
+
+
+//----------------------------THE SPREAD (...) OPERATOR!----------------------
+//We use this to expand an array into all of its elements
+/*
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const goodNewArr = [1, 2, ...arr];
+console.log(goodNewArr)
+console.log(...goodNewArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+*/
+
+//2 Main uses of Spread Operator: Create a shallow copy of an array, merge 2 arrays
+
+//Copying an array:
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+//Join 2 arrays
+/*
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+const str = 'Austin';
+const letters = [...str, '', 'M.'];
+console.log(letters);
+*/
+
+//REAL WORLD EXAMPLE
+/*const ingredients = [prompt('Let\'s make pasta! Ingredient 1?'), prompt('Ingredient 2?'), prompt('Ingredient 3?')]
+
+console.log(ingredients);
+
+restaurant.orderPasta(...ingredients)
+*/
+
+//OBJECT SPREAD OPERATOR
+/*
+const newRestaurant = {
+  ...restaurant,
+  founder: 'Austin McIntyre'
+}
+
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+*/
+
+//-----------REST PATTERN AND PARAMTERS-----------
+
+//DESTRUCTURING
+
+//SPREAD, because (...) is on the RIGHT side of the operator
+const arr = [1, 2, ...[3, 4]];
+
+//REST, because (...) is on the LEFT side of the operator
+const [a, b, ...others] = [1, 2, 3, 4, 5, 6, 8, 15, 87];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+console.log(pizza, risotto, otherFood);
+
+//REST Object
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//FUNCTIONS
+
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+}
+
+add(2, 5);
+add(1, 4, 6);
+add(4, 6, 6, 7, 8)
+
+const x = [23, 5, 7]
+
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+
+restaurant.orderPizza('mushrooms');
